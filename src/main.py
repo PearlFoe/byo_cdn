dc62e5ab1d3e7178343b6aa62f6fbab04f32ec1c
PearlFoe <itknml@gmail.com>
2024-08-23 20:28:06 +0300

    Set cache controle header

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Request, Response
 
 
 router = APIRouter()
 
 @router.get("/")
-async def some_route():
+async def some_route(response: Response):
+    response.headers["Cache-Control"] = "public, max-age=10"
     return {"answer": 42}
 
 
